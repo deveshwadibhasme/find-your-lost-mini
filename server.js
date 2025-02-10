88e2d7becde23937e7d411ea352adaaa35e1bf3b
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-02-10 08:17:25 +0530

    fix/server

--- server.js
+++ server.js
@@ -135,5 +135,5 @@ app.get('/api/missing/search', async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
